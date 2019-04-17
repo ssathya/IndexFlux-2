00378b08dea0fb8cf7551fc5337200baca4e2e2c
Sridhar Sathya <sridhar.sathya@live.com>
2019-04-17 14:10:40 -0400

    Created basic application. Need to test on target server.

---
+++ StockReporter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+namespace StockReporter
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+			logger.Debug("In main");
+			CreateWebHostBuilder(args).Build().Run();
+			logger.Debug("Exiting application");
+		}
+
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>()
+			.ConfigureLogging(logging =>
+			{
+				logging.ClearProviders();
+				logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+			})
+			.UseNLog();
+	}
+}
