d9a2ee6c5f01f575752c903215af2c01e428b4fe
Sridhar Sathya <sridhar.sathya@live.com>
2019-06-14 13:57:54 -0400

    Redirections work. 391 resolved

--- ServeData/Controllers/IntendsHandlerController.cs
+++ ServeData/Controllers/IntendsHandlerController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Google.Apis.Dialogflow.v2.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RestSharp;
 using ServeData.MessageProcessors;
 
 namespace ServeData.Controllers
@@ -41,12 +43,47 @@ namespace ServeData.Controllers
         [HttpPost]
 		public async Task<IActionResult> Post([FromBody] GoogleCloudDialogflowV2WebhookRequest value)
 		{
+			var baseURL = Request.Host.ToString();
+			var keyUsedToAccess = Request.Headers["key"].ToString();
+			
 			string intendDisplayName = value.QueryResult.Intent.DisplayName;
-			if (intendDisplayName.Equals("RandomRecommendation"))
+			if (intendDisplayName.Equals("recommend") || intendDisplayName.Equals("fundamentals"))
 			{
-				RedirectToAction()
+				var client = new RestClient("https://" + baseURL  );
+				var request = new RestRequest(BuildActionMethod(intendDisplayName), Method.POST);
+				request.AddHeader("key", keyUsedToAccess);
+				request.AddJsonBody(value);
+				var response = client.Execute(request);	
+				if (response.IsSuccessful)
+				{
+					var returnMsg = response.Content;
+					return new ContentResult
+					{
+						Content = returnMsg,
+						ContentType = "application/json",
+						StatusCode = 200
+					};
+				}
+				
 			}
 			return await _processMessages.ProcessValuesFromIntents(value);
+		}
+
+		private string BuildActionMethod(string intendDisplayName)
+		{
+			string returnString = "";
+			switch (intendDisplayName)
+			{
+				case "fundamentals":
+					returnString = "/api/Fundamentals";
+					break;
+				case "recommend":
+					returnString = "/api/Recommendations";
+					break;
+				default:
+					break;
+			}
+			return returnString;
 		}
 
 		// PUT: api/IntendsHandler/5
---
+++ ServeData/Controllers/RecommendationsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataProvider.Extensions;
+using Google.Apis.Dialogflow.v2.Data;
+using Google.Cloud.Dialogflow.V2;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServeData.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecommendationsController : ControllerBase
+    {
+       
+        // POST: api/Recommendations
+        [HttpPost]
+        public IActionResult Post([FromBody] GoogleCloudDialogflowV2WebhookRequest value)
+        {
+			string intendDisplayName = value.QueryResult.Intent.DisplayName;
+			var returnValue = new WebhookResponse
+			{
+				FulfillmentText = Utilities.ErrorReturnMsg() + Utilities.EndOfCurrentRequest()
+			};
+			return new ContentResult
+			{
+				Content = returnValue.ToString(),
+				ContentType = "application/json",
+				StatusCode = 200
+			};
+		}
+
+        // PUT: api/Recommendations/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] string value)
+        {
+			return Ok();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
